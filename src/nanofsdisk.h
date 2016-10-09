f6330a55e5c600489624ce8051effcb4c6253d62
Mikhail Gorozhin <m.gorozhin@gmail.com>
2016-10-10 01:53:40 +1000

    Added skeleton for disk communications

--- src/nanofsdisk.h
+++ src/nanofsdisk.h
@@ -7,10 +7,26 @@
 
 typedef FILE* NanoFileSystemDisk;
 
+/*
+  Open a disk from a filename, return a NanoFileSystemDisk
+*/
 NanoFileSystemDisk openDisk(char* fileName);
+
+/*
+  Close NanoFileSystemDisk
+*/
 void closeDisk(NanoFileSystemDisk d);
 
+/*
+  Read a block (BLOCK_SIZE bytes) at a given offset from a disk,
+  Return 0 if everything is Ok, or a non-zero error code
+*/
 char readBlock(NanoFileSystemDisk disk, long offset, void* block);
+
+/*
+  Write a block (BLOCK_SIZE bytes) starting at a given offset to a disk,
+  Return 0 if everything is Ok, or a non-zero error code
+*/
 char writeBlock(NanoFileSystemDisk disk, long offset, void* block);
 
 
